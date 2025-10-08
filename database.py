7093cfbf31f2b4e6c1a811f4db9a7dcb5600e53e
Manoj_Bizamica <manoj.n@bizamica.com>
2025-10-08 12:14:34 +0530

    main.py

--- database.py
+++ database.py
@@ -76,7 +76,7 @@ def create_tables():
 db_was_created = ensure_database_exists()
 
 # Create database URL with psycopg2
-DATABASE_URL = "postgresql+psycopg2://akkani:jJdDTazHBUMsFbNnI5GGTA1iILRUZnWa@dpg-d3iuuhmmcj7s739hsab0-a/akkani_db"
+DATABASE_URL = "postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Create SQLAlchemy engine
 engine = create_engine(
