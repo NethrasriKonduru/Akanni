efd6cc07101db1b93d92b029c58e48c541f79a6c
Manoj_Bizamica <manoj.n@bizamica.com>
2025-10-08 12:11:19 +0530

    main.py

--- main.py
+++ main.py
@@ -125,4 +125,5 @@ async def get_service(service_id: int, db: Session = Depends(get_db)):
     return service
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
